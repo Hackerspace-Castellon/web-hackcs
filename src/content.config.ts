7c4d545becf251385fe150a34abebe0fa0ae2edb
Antonio36996 <antonio36996@gmail.com>
2025-06-09 21:20:32 +0200

    Trying new content.config.ts file

--- src/content.config.ts
+++ src/content.config.ts
@@ -7,6 +7,7 @@ const schema = z.object({
   pubDate: z.coerce.date(),
   updatedDate: z.coerce.date().optional(),
   heroImage: z.string().optional(),
+  categories: z.string().optional(),
 });
 
 const agendaES = defineCollection({
